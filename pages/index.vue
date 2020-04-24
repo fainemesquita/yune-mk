1f197f0f2b71029197c4d98c5f8247e05da5f681
MK-Yune <yune.mk899@gmail.com>
2020-04-24 17:11:31 +0900

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="index">
+    <card
+      v-for="(post, i) in posts"
+      :id="post.sys.id"
+      :key="i"
+      :title="post.fields.title"
+      :date="post.sys.updatedAt"
+    />
+  </section>
+</template>
+
+<script>
+import Card from '~/components/card.vue'
+import { createClient } from '~/plugins/contentful.js'
+
+const client = createClient()
+export default {
+  transition: 'slide-left',
+  components: {
+    Card
+  },
+  asyncData({ env, params }) {
+    return client
+      .getEntries(env.CTF_BLOG_POST_TYPE_ID)
+      .then((entries) => {
+        return {
+          posts: entries.items
+        }
+      })
+      .catch(console.error)
+  }
+}
+</script>
